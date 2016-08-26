08568c2f5e09eb81cdeb4c614280a0aa1bf78e78
estrattonbailey <ericstrattonbailey@gmail.com>
2016-08-25 22:51:58 -0400

    Package UMD

--- browser/sharable.js
+++ browser/sharable.js
@@ -1,4 +1,4 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.overunder = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.sharable = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
@@ -22,20 +22,23 @@ var networks = {
  * new object
  *
  * @param {object} target Root object
- * @param {object} merge Object to merge 
+ * @param {object} source Object to merge 
  *
  * @return {object} A *new* object with all props of the passed objects
  */
-var merge = function merge(target, _merge) {
-  var result = {};
-  var props = Object.keys(target);
-
-  for (var i = 0; i < props.length; i++) {
-    var prop = props[i];
-    result[prop] = _merge[prop] ? _merge[prop] : target[prop];
+var merge = function merge(target) {
+  for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+    args[_key - 1] = arguments[_key];
   }
 
-  return result;
+  for (var i = 0; i < args.length; i++) {
+    var source = args[i];
+    for (var key in source) {
+      if (source[key]) target[key] = source[key];
+    }
+  }
+
+  return target;
 };
 
 /**
@@ -62,11 +65,12 @@ var getMeta = function getMeta() {
   var head = document.head;
   var metaTags = [].slice.call(head.getElementsByTagName('meta'));
 
-  for (var _i = 0; _i < metaTags.length; _i++) {
-    var attributes = [].slice.call(metaTags[_i].attributes);
+  for (var i = 0; i < metaTags.length; i++) {
+    var attributes = [].slice.call(metaTags[i].attributes);
 
-    for (var _i = 0; _i < attributes.length; _i++) {
-      var attr = attributes[_i];
+    for (var a = 0; a < attributes.length; a++) {
+      var attr = attributes[a];
+
       /** 
        * If a Twitter or Open Graph tag 
        */
@@ -95,9 +99,15 @@ var getMeta = function getMeta() {
  * @return {object} Object with name of network and any optional override values
  */
 var parseLocalData = function parseLocalData(target, attribute) {
-  var raw = target.getAttribute(attribute).split(/\s/);
-  var network = raw[0];
-  var overrides = raw[1] ? JSON.parse(opts[1]) : null;
+  var network = void 0;
+  var overrides = {};
+  var attributes = Array.prototype.slice.call(target.attributes);
+
+  for (var i = 0; i < attributes.length; i++) {
+    var attr = attributes[i];
+    var key = attr.name.split(/data-/)[1] || attr.name;
+    attr.name === attribute ? network = attr.value : overrides[key] = attr.value;
+  }
 
   return {
     network: network,
@@ -111,22 +121,15 @@ var parseLocalData = function parseLocalData(target, attribute) {
  *
  * @return {string} The complete URL based on the template
  */
-function createURL(network, meta) {
+var createURL = function createURL(network, meta) {
   var params = networks[network].split(/\|/);
 
   var replace = function replace(string, value) {
-    return string.replace(/{{.*?}}/g, function (str) {
-      return encodeURI(value);
-    });
+    return string.replace(/\{\{.*?\}\}/g, encodeURI(value));
   };
 
-  /**
-   * Iterate over each query parameter 
-   * in URL. If we have data, insert it.
-   * If not, remove the query parameter.
-   */
   for (var i = 1; i < params.length; i++) {
-    var type = params[i].split(/{{|}}/g)[1];
+    var type = params[i].split(/\{\{|\}\}/g)[1];
 
     if (meta[type]) {
       params[i] = replace(params[i], meta[type]);
@@ -137,38 +140,75 @@ function createURL(network, meta) {
   }
 
   return params.join('');
-}
+};
 
 /**
  * @param {object} config 
  */
-function Sharable(config) {
+function Sharable() {
+  var config = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+
   var options = merge({
     selector: 'data-social'
   }, config);
 
-  var meta = getMeta();
-  var targets = [].slice.call(document.querySelectorAll('[' + options.selector + ']'));
+  var instance = Object.create({
+    options: options,
+    update: update
+  }, {
+    meta: {
+      value: getMeta(),
+      writable: true
+    }
+  });
 
-  var _loop = function _loop(i) {
-    var target = target[i];
+  /**
+   * Init
+   */
+  bindLinks();
 
-    target.onclick = function (e) {
-      e.preventDefault();
+  return instance;
+
+  /**
+   * Bind new links,
+   * fetch new data from head
+   */
+  function update() {
+    bindLinks();
+    instance.meta = getMeta();
+  }
+
+  /**
+   * Binds links with configured data-attribute selector,
+   * then removes the attribute to prevent rebinding
+   */
+  function bindLinks() {
+    var targets = [].slice.call(document.querySelectorAll('[' + options.selector + ']')) || [];
+
+    if (targets.length < 1) return;
+
+    var _loop = function _loop(i) {
+      var target = targets[i];
 
       var _parseLocalData = parseLocalData(target, options.selector);
 
       var network = _parseLocalData.network;
       var overrides = _parseLocalData.overrides;
 
-      var meta = merge(data, overrides);
-      var url = createURL(network, meta);
-      openPopup(url);
-    };
-  };
+      var url = createURL(network, merge(instance.meta, overrides));
 
-  for (var i = 0; i < targets.length; i++) {
-    _loop(i);
+      target.addEventListener('click', function () {
+        return openPopup(url);
+      });
+
+      target.removeAttribute(options.selector);
+    };
+
+    for (var i = 0; i < targets.length; i++) {
+      _loop(i);
+    }
+
+    return targets;
   }
 }
 
